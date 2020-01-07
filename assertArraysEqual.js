832f71a96c52327c78062a2d79ea5e7a943fc3ee
Henry Boyd <AmorphousCube@gmail.com>
2020-01-07 17:08:44 +0000

    added file which checks to see if two arrays are exactly equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+//Functions
+const eqArrays = function (arrayActual, arrayExpected) {
+  for (let i = 0; i < arrayActual.length; i++) {
+    if (!(arrayActual[i] === arrayExpected[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEqual = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅ Assertion passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+}
+
+// assertArrayEqual([1,2],[1,2]);
+// assertArrayEqual([1,2],[1,'2']);
+// assertArrayEqual([1,2],[1]);
+// assertArrayEqual([],[]);
+// assertArrayEqual([1,2,3],[1,2]);
+// assertArrayEqual([1,2,3],[1,3,2]);
