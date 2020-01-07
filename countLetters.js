adef65714e54e393f844486e91e3ee4ac52529dd
Henry Boyd <AmorphousCube@gmail.com>
2020-01-07 21:05:28 +0000

    eslint fix

--- countLetters.js
+++ countLetters.js
@@ -8,20 +8,20 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   string = string.split(' ').join('');
-  let result = {}
-  for (let element of string){
-    if (result[element]){
+  let result = {};
+  for (let element of string) {
+    if (result[element]) {
       result[element] += 1;
     } else {
       result[element] = 1;
     }
   }
   return result;
-}
+};
 
-console.log(countLetters("bagels are good")); 
-console.log(countLetters("numb3rs4r3h4rd")); 
-console.log(countLetters("")); 
-console.log(countLetters(" ")); 
-console.log(countLetters("aaaaaa")); 
-console.log(countLetters("aaaaaaaAAAAAAAA!")); 
+console.log(countLetters("bagels are good"));
+console.log(countLetters("numb3rs4r3h4rd"));
+console.log(countLetters(""));
+console.log(countLetters(" "));
+console.log(countLetters("aaaaaa"));
+console.log(countLetters("aaaaaaaAAAAAAAA!"));
--- countOnly.js
+++ countOnly.js
@@ -20,7 +20,7 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-}
+};
 
 
 const firstNames = [
